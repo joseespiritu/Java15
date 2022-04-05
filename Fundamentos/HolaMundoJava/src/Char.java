265f5e84773ca1fda5de50602909bc9e5474c9ea
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-04-04 23:04:28 -0500

    comment java 15

--- Fundamentos/HolaMundoJava/src/Char.java
+++ Fundamentos/HolaMundoJava/src/Char.java
@@ -1,32 +1,32 @@
 // Tipo char
 public class Char {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         char miCaracter = 'a';
         System.out.println("miCaracter = " + miCaracter);
-        
+
         // Utilizando valor unicode
         char varChar = '\u0021';
         System.out.println("varChar = " + varChar);
-        
+
         char varCharDecimal = 33;
         System.out.println("varCharDecimal = " + varCharDecimal);
-        
+
         char varCharSimbolo = '!';
         System.out.println("varCharSimbolo = " + varCharSimbolo);
-        
+
         // Trabajando con var JDK 15
-        var varChar1 = '\u0021';
-        System.out.println("varChar1 = " + varChar1);
-        
-        var varCharDecimal2 = (char)33;
-        System.out.println("varCharDecimal2 = " + varCharDecimal2);
-        
-        var varCharSimbolo1 = '!';
-        System.out.println("varCharSimbolo1 = " + varCharSimbolo1);
-        
+        // var varChar1 = '\u0021';
+        // System.out.println("varChar1 = " + varChar1);
+
+        // var varCharDecimal2 = (char)33;
+        // System.out.println("varCharDecimal2 = " + varCharDecimal2);
+
+        // var varCharSimbolo1 = '!';
+        // System.out.println("varCharSimbolo1 = " + varCharSimbolo1);
+
         int variableEnteraSimbolo = '!';
         System.out.println("variableEnteraSimbolo = " + variableEnteraSimbolo);
-        
+
         int letra = 'A';
         System.out.println("letra = " + letra);
     }
