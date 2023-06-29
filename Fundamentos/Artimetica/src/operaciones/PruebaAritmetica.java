83b9de2da8de40aea896c50b8e2d308006409606
joseespiritu <joseespiritu89@aragon.unam.mx>
2023-06-29 17:31:55 -0500

    Constructores

--- Fundamentos/Artimetica/src/operaciones/PruebaAritmetica.java
+++ Fundamentos/Artimetica/src/operaciones/PruebaAritmetica.java
@@ -3,14 +3,21 @@ package operaciones;
 public class PruebaAritmetica {
     public static void main(String[] args) {
         Aritmetica aritmetica1 = new Aritmetica();
-        aritmetica1.a = 3;
-        aritmetica1.b = 2;
-        aritmetica1.sumar();
+        System.out.println("aritmetica1 a:" + aritmetica1.a);
+        System.out.println("aritmetica1 b:" + aritmetica1.b);
         
-        var resultado = aritmetica1.sumarConRetorno();
-        System.out.println("resultado desde la prueba = " + resultado);
+        Aritmetica aritmetica2 = new Aritmetica(5, 8);
+        System.out.println("aritmetica2 a:" + aritmetica2.a);
+        System.out.println("aritmetica2 b:" + aritmetica2.b);
         
-        resultado = aritmetica1.sumarConArgumentos(5, 8);
-        System.out.println("resultado usando argumentos = " + resultado);
+//        aritmetica1.a = 3;
+//        aritmetica1.b = 2;
+//        aritmetica1.sumar();
+//        
+//        var resultado = aritmetica1.sumarConRetorno();
+//        System.out.println("resultado desde la prueba = " + resultado);
+//        
+//        resultado = aritmetica1.sumarConArgumentos(5, 8);
+//        System.out.println("resultado usando argumentos = " + resultado);
     }
 }
